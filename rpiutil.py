7920c0e80869d067c401f6191269697c10923954
akd001 <akd001@users.noreply.github.com>
2016-12-15 02:30:42 +0530

    output to data folder

--- rpiutil.py
+++ rpiutil.py
@@ -2,7 +2,7 @@ import datetime
 import time
 
 def logOutput (fileName, logString):
-	with open("../outputLog.txt", "w") as outputFile:
+	with open("../data/outputLog.txt", "w") as outputFile:
 		ts = time.time()
 		dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 		outputFile.write(dt + ": " + fileName + " - " + logString)
