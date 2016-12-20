859a2e474c357f2ae140e59c0d6f6cd564b03cf1
akd001 <001.amby@gmail.com>
2016-12-20 08:46:32 +0530

    fix gitPull, add home folder

--- gitPull.py
+++ gitPull.py
@@ -2,5 +2,5 @@ import subprocess
 import rpiutil
 
 def pull():
-	outString = subprocess.check_output("git -C git/ pull", shell=True)
+	outString = subprocess.check_output("git -C " + rpiutil.getHome() + "git/ pull", shell=True)
 	rpiutil.logOutput("gitPull", outString)
--- rpiutil.py
+++ rpiutil.py
@@ -2,7 +2,10 @@ import datetime
 import time
 
 def logOutput (fileName, logString):
-	with open("../data/outputLog.txt", "a") as outputFile:
+	with open(getHome() + "data/outputLog.txt", "a") as outputFile:
 		ts = time.time()
 		dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 		outputFile.write(dt + ": " + fileName + " - " + logString)
+
+def getHome ():
+	return '/home/akd/'
