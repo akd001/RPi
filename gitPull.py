3d0215255ce6bce03023c9a0e2c6427ea46d2339
akd001 <001.amby@gmail.com>
2016-12-20 08:38:38 +0530

    fix gitPull

--- gitPull.py
+++ gitPull.py
@@ -2,5 +2,5 @@ import subprocess
 import rpiutil
 
 def pull():
-	outString = subprocess.check_output("git pull", shell=True)
+	outString = subprocess.check_output("git -C git/ pull", shell=True)
 	rpiutil.logOutput("gitPull", outString)
