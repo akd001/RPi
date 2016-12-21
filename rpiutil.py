89fde73b3d441e4056b216affca354d3128e6ad3
akd001 <akd001@users.noreply.github.com>
2016-12-21 14:11:40 +0530

    rpiutil changes for data dictionary, mail

--- rpiutil.py
+++ rpiutil.py
@@ -1,5 +1,10 @@
-import datetime
-import time
+def getHome ():
+	return '/Users/ambar/Desktop/Workspace/Rpi/'
+
+sys.path.insert(0, getHome() + 'data')
+import sys, datetime, time, smtplib
+from email.mime.text import MIMEText
+import data
 
 def logOutput (fileName, logString):
 	with open(getHome() + "data/outputLog.txt", "a") as outputFile:
@@ -7,5 +12,22 @@ def logOutput (fileName, logString):
 		dt = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 		outputFile.write(dt + ": " + fileName + " - " + logString)
 
-def getHome ():
-	return '/home/akd/'
+def getMailData (dataKey):
+	return data.mailData[dataKey]
+
+def getSmtpServer ():
+	smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
+	smtpserver.ehlo()
+	smtpserver.starttls()
+	smtpserver.ehlo()
+	smtpserver.login(getMailData('mailUser'), getMailData('mailPass'))
+	return smtpserver
+
+def sendMail (mailSubject, mailBody):
+	msg = MIMEText(mailBody)
+	msg['Subject'] = mailSubject
+	msg['From'] = getMailData('mailUserName')
+	msg['To'] = getMailData('mailTo')
+	smtpServer = getSmtpServer()
+	smtpServer.sendmail(gmail_user, [getMailData('mailTo')], msg.as_string())
+	smtpServer.quit()
